96d1d1d34ffc995bac1a43ae3e17b6557cad34a0
OG <OG@bcts.com>
2021-02-25 19:12:32 +0300

    Dev branch 1-st commit

--- App.js
+++ App.js
@@ -1,10 +1,13 @@
 import React from "react"
-import {View} from "react-native"
+import {View, Text} from "react-native"
 
 class App extends React.Component{
   render(){
     return (
       <View/>
+
+
+      
     )
   }
 }
