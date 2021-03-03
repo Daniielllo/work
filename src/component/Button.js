eca6060f181b03d4db63e02bf21160b19b0dafbd
Spanch <dlapynkov@mail.ru>
2021-03-03 16:22:05 +0300

    NOTendedHeadScroll

--- src/component/Button.js
+++ src/component/Button.js
@@ -11,38 +11,4 @@ class Button extends React.Component{
         )
     }
 }
-
-export class ButtonDone extends React.Component{
-    
-    render(){
-        return(
-            <TouchableOpacity onPress={this.props.updatePeople}>
-                <Text>Done</Text>
-            </TouchableOpacity>
-        )
-    }
-
-}
-
-export class ButtonNext extends React.Component{
-    render(){
-        return(
-            <TouchableOpacity onPress={this.props.increacse}>
-                <Text>Next</Text>
-            </TouchableOpacity>
-        )
-    }
-}
-
-
-export class ButtonPrev extends React.Component{
-    render(){
-        return(
-            <TouchableOpacity onPress={this.props.decreacse}>
-                <Text>Prev</Text>
-            </TouchableOpacity>
-        )
-    }
-}
-
 export default Button;
---
+++ src/component/HeadScroll.js
@@ -0,0 +1,30 @@
+import React from "react";
+import { ScrollView } from "react-native";
+import Button from "./Button";
+
+class HeadScroll extends React.Component{
+    state = {
+        categorys:[
+            {
+                name:
+            },
+            {
+                name:
+            },
+            {
+                name:
+            }
+
+        ]
+    }
+    render(){
+
+        return(
+            <ScrollView horizontal={true}>
+                 
+            </ScrollView>
+        )
+    }
+}
+
+export default HeadScroll;
