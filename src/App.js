9deb5fcc7772fadb6a264fe02010492a71a1cb0f
OG <OG@bcts.com>
2021-03-04 20:21:13 +0300

    Complited

--- src/App.js
+++ src/App.js
@@ -6,12 +6,19 @@ import ScrollDown from "./component/ScrollDown";
 
 
 class App extends React.Component {
+    state = {
+        activeCategoryid: 0
+    }
     
+Change = (activeCategoryid) => {
+    this.setState({activeCategoryid})
+}
+
     render() {
         return (
             <View style={{flex:1,alignItems:'center',justifyContent:'center'}}>
-                <HeadScroll/>
-                <ScrollDown/>
+                <HeadScroll Change={this.Change} activeCategoryid={this.state.activeCategoryid}/>
+                <ScrollDown activeCategory={this.state.activeCategoryid}/>
             </View>
             
         )
