2f6b62d4ab2c0bf474fdd861850a38949ef73c42
Spanch <dlapynkov@mail.ru>
2021-03-08 14:45:54 +0300

    NonStyledBottomInput

--- src/App.js
+++ src/App.js
@@ -2,17 +2,18 @@ import React from 'react';
 import { Text, View } from "react-native";
 import Footer from "./component/Footer";
 
+const temp = [0]
 
 class App extends React.Component {
     state = {
         main: [
-            1,
             ""
         ]
     }
     
     Send = (TextInput) =>{
-        this.state.main[main[0]] = TextInput
+        temp[0] = main[0]
+        this.state.main[temp[0]] = TextInput
         this.state.main[0]++
     }
     
@@ -20,7 +21,7 @@ class App extends React.Component {
 
     render() {
         return (
-            <View style={{ justifyContent: "center", alignItems: "center"}}>
+            <View style={{ justifyContent: "center", alignItems: "center", flex:1}}>
                 <Footer Send={this.Send}/>
             </View>
         )
--- src/component/Footer.js
+++ src/component/Footer.js
@@ -1,6 +1,9 @@
 import React from 'react';
-import { TextInput, View } from "react-native";
-import { ButtonSeed } from "./supporters/ButtonSeed"
+import { TextInput, View, Dimensions, ScrollView } from "react-native";
+import CheckButton from './supporters/CheckButton';
+
+const windowWidth = Dimensions.get('window').width;
+const windowHeight = Dimensions.get('window').height * 0.7;
 
 const tempText = [""]
 
@@ -11,21 +14,23 @@ class Footer extends React.Component {
 
         return (
 
-            <View style={{
-                justifyContent: "space-between",
-                alignItems: "flex-start"
+            <ScrollView horizontal style={{
+                height: 150,
+                width: windowWidth,
+                position: "absolute",
+                top: windowHeight,
             }}>
                 <TextInput
                     style={{
                         height: 40,
-                        width: 100,
+                        width: 230,
                         borderColor: '#000000',
                         borderWidth: 1,
                     }}
                     onChangeText={text => tempText[0] = text}
                 />
-                <ButtonSeed Send={Send} Text={tempText} />
-            </View>
+                <CheckButton style={{}} Send={Send} Text={tempText} />
+            </ScrollView>
         )
     }
 }
