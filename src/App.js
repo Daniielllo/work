e037b719e3347ad405051f046f9b386c1ef09677
Spanch <dlapynkov@mail.ru>
2021-03-12 13:49:58 +0300

    DontKnowHowChangeByedOnState

--- src/App.js
+++ src/App.js
@@ -5,97 +5,119 @@ import ScrollDown from './component/ScrollDown';
 
 let mass = []
 
+let tempom = {}
+
 class App extends React.Component {
     state = {
         main: [
-            
+
             {
                 line: "___________1",
-                byed: false,
+                byed: "false",
                 id: 0
             },
             {
                 line: "___________2",
-                byed: false,
+                byed: "false",
                 id: 1
             },
             {
                 line: "___________3",
-                byed: false,
+                byed: "false",
                 id: 2
             },
             {
                 line: "___________4",
-                byed: false,
+                byed: "false",
                 id: 3
             },
             {
                 line: "___________5",
-                byed: false,
+                byed: "false",
                 id: 4
             },
             {
                 line: "___________6",
-                byed: false,
+                byed: "false",
                 id: 5
             },
             {
                 line: "___________7",
-                byed: false,
+                byed: "false",
                 id: 6
             },
             {
                 line: "___________8",
-                byed: false,
+                byed: "false",
                 id: 7
             },
-            // {
-            //     line: "___________9",
-            //     byed: false,
-            //     id: 8
-            // },
-            // {
-            //     line: "___________10",
-            //     byed: false,
-            //     id: 9
-            // },
-            // {
-            //     line: "___________11",
-            //     byed: false,
-            //     id: 10
-            // }
         ],
         size: -1,
         temptext: {
-            line: "",
-            byed: false,
+            line: "__________",
+            byed: "false",
             id: 0
         },
     }
 
     ChangeText = (text) => {
-        this.setState({temptext: {
-            line:text,
-            byed: false,
-            id:this.state.size + 1
-        } })
+        this.setState({
+            temptext: {
+                line: text,
+                byed: "false",
+                id: this.state.size + 1
+            }
+        })
     }
 
-    Send = () =>{
-        const { main, temptext }  = this.state
+    Buy = (itemId) => {
+        const { main } = this.state
         mass = main
-        this.setState({size: this.state.size + 1})
-        mass[this.state.size + 1] = temptext
-        this.setState({main: mass})
+        let leto = mass.find(item => item.id == itemId)
+        
+        mass[leto] = tempom
+
+        if (mass[leto]["byed"] == "false"){
+            tempom = {
+                line: "__________",
+                byed: "true",
+                id: 0
+            }
+        }
+        else
+        {
+            tempom = {
+                line: "__________",
+                byed: "false",
+                id: 0
+            }
+        }
+        this.setState({ main: mass })
     }
-    
+
+    Send = () => {
+        const { main, temptext } = this.state
+        mass = main
+        if (this.state.size === mass.length - 2) {
+            mass.push({
+                line: "___________",
+                byed: "false",
+                id: this.state.size + 2
+            })
+
+        }
+        this.setState({ size: this.state.size + 1 })
+        mass[this.state.size + 1] = temptext
+        this.setState({ main: mass })
+    }
+
 
 
     render() {
         return (
-            <View style={{ justifyContent: "center", alignItems: "center", flex:1}}>
-                <ScrollDown mass={this.state.main}/>
-                <Footer Send={this.Send} ChangeText={this.ChangeText}/>
+            <View style={{ justifyContent: "center", alignItems: "center", flex: 1 }}>
+                <ScrollDown mass={this.state.main} Buy={this.Buy} />
+                <Footer Send={this.Send} ChangeText={this.ChangeText} />
             </View>
         )
     }
--- src/component/ScrollDown.js
+++ src/component/ScrollDown.js
@@ -1,19 +1,64 @@
 import React from 'react';
-import { FlatList, Dimensions } from 'react-native';
-import { renderItem } from './supporters/helpers';
+import { FlatList, Dimensions, View, TouchableOpacity, Text, ScrollView } from 'react-native';
+
 const windowHeight = Dimensions.get('window').height;
+const windowWidth = Dimensions.get('window').width;
+
+// const Item = ({ item, Buy }) => (
+//     <View style={{ height: windowHeight * 0.12, width: windowWidth }}>
+//         <TouchableOpacity style={{
+//             padding: 30,
+//             top: 80,
+//             backgroundColor: "#ffcc00",
+//             color: "#ffffff",
+
+//         }} onPress={() => { Buy(item.id) }}>
+//             <Text style={{ textDecorationLine: item.byed == true ? "line-through" : "none" }}>{item.line}</Text>
+//         </TouchableOpacity>
+
+//     </View>
+// );
+
+// const renderItem = ({ item, Buy }) => (
+//     <Item item={item} Buy={Buy} />
+// );
 
 class ScrollDown extends React.Component {
 
-    render(){
-        const { mass } = this.props
-        return(
-            <FlatList
-                style={{bottom:windowHeight*0.10}}
-                data={mass}
-                renderItem={renderItem}
-                keyExtractor={item => item.id}
-            />
+    render() {
+        const { mass, Buy } = this.props
+        return (
+            <ScrollView style={{ bottom: windowHeight * 0.10 }}>
+                {
+                    mass.map((item) => (
+                        <View style={{ height: windowHeight * 0.12, width: windowWidth }}>
+                            <TouchableOpacity style={{
+                                padding: 30,
+                                top: 80,
+                                backgroundColor: "#ffcc00",
+                                color: "#ffffff",
+
+                            }} onPress={Buy(item.id)}>
+                                <Text style={{
+                                    textDecorationLine: item.byed == "true" ? "line-through" : "none"
+                                }}>
+                                    {item.byed}
+                                </Text>
+                            </TouchableOpacity>
+
+                        </View>
+                    ))
+                }
+
+            </ScrollView>
+
+
+            // <FlatList
+            //     style={{bottom:windowHeight*0.10}}
+            //     data={mass}
+            //     renderItem={renderItem}
+            //     keyExtractor={item => item.id}
+            // />
         )
     }
 }
